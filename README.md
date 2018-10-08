fec774b76ef236e6cb97a39e79fecde29d1c1789
Koen Geerts <Koen.Geerts@realdolmen.com>
2018-10-08 16:24:36 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# RTCM
+Real Time Crowd Management API
+
+To use the RTCM Bin Viewer you need to have an account on enco.io and have a registered access to the Real Time Crowd Management APIs (RTCM).
+
+Open the RTCM microsite from enco.io (rtcm.enco.io), create a new zone.
+Go to http://kara.rest, and create a new bin. Copy the http address of the paste bin. 
+Back in RTCM, create a new stream for your zone, and use your HTTP paste bin from kara.rest to send all data. 
+Download and unzip the RTCMbinviewer.zip archive on your local computer, and review the binviewer.html to add your own Google Maps API keys if you don't want the Google demo disclaimer on your maps.
+Double click the binviewer.html file to open the tool in your browser, and use the kara.rest http paste bin address from above.
+You can change the overlay opacity as well as heatmap colors and heatmap step thresholds.
+
+This code is provided for demonstration and development only and cannot be used for production systems.
---
+++ binviewer/js/main.js
@@ -0,0 +1,260 @@
+var reproject = require('reproject');
+var epsg = require('epsg');
+
+// Globals
+var map;
+var binUrl;
+var binApiUrl;
+var heatmapValues;
+var heatmapColors;
+var opacity = 80;
+
+$("#opacityRange").on('input propertychange', function () {
+    opacity = $(this).val();
+    $("#opacityValue").text(opacity);
+    saveColors();
+});
+
+$("#colorInput").change(function () {
+    if (validateHeatmapInput()) {
+        saveColors();
+        createHeatmap();
+    }
+});
+
+$("#scaleInput").change(function () {
+    if (validateHeatmapInput()) {
+        saveColors();
+        createHeatmap();
+    }
+});
+
+$("#binUrlField").change(function () {
+    binUrl = $(this).val();
+    saveBinUrl();
+    redrawMap();
+});
+
+$("#refresh").click(function () {
+    redrawMap();
+});
+
+function refreshLastUpdateTimes(lastBindUpdate) {
+    $("#lastRefresh").text(moment().format("DD/M/YYYY HH:mm:ss"));
+    $("#lastBinUpdate").text(moment(lastBindUpdate).format("DD/M/YYYY HH:mm:ss"));
+}
+
+function validateHeatmapInput() {
+    var colors = $("#colorInput").val().trim().replace(/ /g, "").split(",");
+    var values = $("#scaleInput").val().trim().split(",").map(function (item) {
+        return parseInt(item);
+    });
+
+    var error = false;
+    var errorMsg;
+
+    if (colors.length !== values.length) {
+        error = true;
+        errorMsg = "Provide an equal amount of colors and range values."
+    } else if (!validColors(colors)) {
+        error = true;
+        errorMsg = "Provide valid hex colors."
+
+    } else if (!validRanges(values)) {
+        error = true;
+        errorMsg = "Provide a valid range of ascending integer values."
+    }
+
+    if (error) {
+        $("#scaleErrorMsg").text(errorMsg);
+        $("#scaleError").show();
+        return false;
+    } else {
+        $("#scaleError").hide();
+        heatmapColors = colors;
+        heatmapValues = values;
+        return true;
+    }
+}
+
+function validColors(array) {
+    for (var i = 0; i < array.length; i++) {
+        var isValidHexColor  = /^#[0-9A-F]{6}$/i.test(array[i]);
+        if (!isValidHexColor) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function validRanges(array) {
+    for (var i = 0; i < array.length; i++) {
+        if (!$.isNumeric(array[i])) {
+            return false;
+        }
+
+        if (i > 0 && parseInt(array[i - 1]) > parseInt(array[i])) {
+            return false
+        }
+    }
+    return true;
+}
+
+function createHeatmap() {
+    $("#scaleLabels").empty();
+    heatmapValues.forEach(function (value, index, array) {
+        var text;
+        if (index === 0) {
+            text = "<" + heatmapValues[index];
+        } else if (index === array.length - 1) {
+            text = heatmapValues[index - 1] + " - " + heatmapValues[index] + "+";
+        } else {
+            text = heatmapValues[index - 1] + " - " + heatmapValues[index];
+        }
+        var labelColor = heatmapColors[index];
+
+        var fontColor = retrieveFontColor(labelColor);
+
+        $("#scaleLabels").append(`<div class='ui horizontal label' style='background-color: ${labelColor}; color: ${fontColor}'>${text}</div>`);
+    });
+}
+
+function retrieveFontColor(labelColor) {
+    var rgbColor = hexToRgb(labelColor);
+    var hsp = Math.sqrt(
+        0.299 * (rgbColor.r * rgbColor.r) +
+        0.587 * (rgbColor.g * rgbColor.g) +
+        0.114 * (rgbColor.b * rgbColor.b)
+    );
+
+    if (hsp > 127.5) {
+        return "black";
+    } else {
+        return "white";
+    }
+}
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+function saveColors() {
+    map.data.setStyle(function (feature) {
+        var national = feature.getProperty('national');
+        var color;
+
+        if (national == null) {
+            color = 'gray';
+        } else {
+            var i = 0;
+            while (national > heatmapValues[i] && i < heatmapValues.length - 1) {
+                i = i + 1;
+            }
+            color = heatmapColors[i];
+        }
+        return {
+            fillColor: color,
+            strokeWeight: 1,
+            fillOpacity: opacity / 100
+        };
+    });
+}
+
+function saveBinUrl() {
+    if (!binUrl.trim().startsWith("https://kara.rest/bin/")) {
+        $("#errorLabel").show();
+    } else {
+        $("#errorLabel").hide();
+        var strippedBinId = binUrl.trim().replace("https://kara.rest/bin/", "").replace("/", "");
+        binApiUrl = "https://kara.rest/api/v1/bins/" + strippedBinId + "/requests?fields=body,requestTime&limit=1";
+    }
+}
+
+function drawLambertWKTWithData(wkt, map, national, international, size) {
+    var geoJson = wellknown.parse(wkt);
+    var feature = {
+        type: "Feature",
+        geometry: geoJson,
+        properties: {national: national, international: international, size: size}
+    };
+    map.data.addGeoJson(reproject.reproject(feature, 'EPSG:3035', 'EPSG:4326', epsg));
+}
+
+function initAll() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: {lat: 51.025183, lng: 3.894707}
+    });
+    validateHeatmapInput();
+    createHeatmap();
+    saveColors();
+    redrawMap();
+    setInterval(function () {
+        redrawMap();
+    }, 30000); // Autorefresh every 30s
+}
+
+function redrawMap() {
+    var data = new XMLHttpRequest();
+    data.onreadystatechange = function () {
+        if (data.readyState === XMLHttpRequest.DONE) {
+            var jsonResponse = JSON.parse(data.responseText);
+            if (jsonResponse[0] === undefined) {
+                return;
+            }
+            var newData = JSON.parse(jsonResponse[0].body);
+            map.data.forEach(function (feature) {
+                // If you want, check here for some constraints.
+                map.data.remove(feature);
+            });
+            newData.forEach(function (cell) {
+                var counter = 0;
+                var cellSplit = [];
+                var pow = false;
+                if (cell.binId.includes("km")) {
+                    cellSplit = /([^k]+)kmE([^N]+)N(.*)/g.exec(cell.binId);
+                    counter = 3;
+                    pow = true;
+                } else {
+                    cellSplit = /([^m]+)mE([^N]+)N(.*)/g.exec(cell.binId);
+                }
+                var tempCell = cellSplit[1];
+                while (tempCell % 10 === 0) {
+                    counter++;
+                    tempCell = tempCell / 10;
+                }
+                var step;
+                if (pow) {
+                    step = cellSplit[1] * Math.pow(10, counter);
+                } else {
+                    step = cellSplit[1];
+                }
+                var east = cellSplit[2] * Math.pow(10, counter);
+                var north = cellSplit[3] * Math.pow(10, counter);
+
+
+                var wkt = "POLYGON((" + east + " " + north + ","
+                    + (+east + +step) + " " + north + ","
+                    + (+east + +step) + " " + (+north + +step) + ","
+                    + east + " " + (+north + +step) + ","
+                    + east + " " + north +
+                    "))";
+
+
+                drawLambertWKTWithData(wkt, map, cell.national, cell.international, step);
+                refreshLastUpdateTimes(jsonResponse[0].requestTime);
+            });
+        }
+    };
+
+    if (binApiUrl != null) {
+        data.open('GET', binApiUrl, true);
+        data.setRequestHeader("Accept", "application/json");
+        data.send(null);
+    }
+}
